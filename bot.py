3fd01e4ddbf4452ccadc5447e615e9c5a73ab399
Infqq <57950063+Infqq@users.noreply.github.com>
2020-04-19 10:44:36 +0400

    v1.1
    Добавлена опция отключения общения. Если speak = 1, то бот разговаривает, если иное значение, то бот отвечает "Команда не найдена."

--- bot.py
+++ bot.py
@@ -4,9 +4,14 @@ import requests
 
 bot=Bot('token')
 
+speak = 1
+
 @bot.on.message(text='<name>')
 async def wrapper(ans: Message, name):
-    response = requests.get("https://isinkin-bot-api.herokuapp.com/1/talk?q="+name.replace(" ","%20")).text[9:-2]
-    await ans(f'{response}')
+    if speak == 1:
+        response = requests.get("https://isinkin-bot-api.herokuapp.com/1/talk?q="+name.replace(" ","%20")).text[9:-2]
+        await ans(f'{response}')
+    else:
+        await ans('Команда не найдена.')
 
 bot.run_polling()
